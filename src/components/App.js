35c9c6bb025addf28f7899e649f0012fc840109b
Ontaria Woods <ontaria.woods@gmail.com>
2023-04-08 20:14:58 -0400

    commit tree c3ae68f7374610b03041a5d9c9eaddaef7289ec0

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React, { useEffect, useState } from "react";
+import { data } from "../mocks/data";
+
+function App() {
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setImage(data.message));
+  }, []);
+
+  if (!image) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={image} alt="A Random Dog"></img>
+    </div>
+  );
+}
+
+export default App;
